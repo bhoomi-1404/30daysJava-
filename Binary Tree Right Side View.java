af955ae5fcd3b1d1c1b79d32220cd467faf40291
bhoomi-1404 <89828292+bhoomi-1404@users.noreply.github.com>
2022-03-05 15:13:17 +0530

    LeetCode 199

---
+++ Binary Tree Right Side View.java
@@ -0,0 +1,22 @@
+class Solution {
+    void solve(TreeNode root,ArrayList<Integer> ans,int level)
+    {
+        if(root==null)
+        {
+            return ;
+        }
+        if(level==ans.size())
+        {
+            ans.add(root.val);
+        }
+        solve(root.right,ans,level+1);
+        solve(root.left,ans,level+1);
+        
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList <Integer> ans=new ArrayList<>();
+        solve(root,ans,0);
+        return ans;
+        
+    }
+}
